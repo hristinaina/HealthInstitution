002c0ca594032074c9ff38ec1a88573798351098
Milica Sladaković <milicasladakovic7@gmail.com>
2022-04-20 10:01:32 +0200

    Resolved merge conflicts

--- HealthInstitution/App.xaml.cs
+++ HealthInstitution/App.xaml.cs
@@ -18,33 +18,32 @@ namespace HealthInstitution
         {
             // * appSettings kao parametre prima putanje do fajlova gdje su smjesteni podaci za rad Zdravstvene Ustanove 
             AppSettings appSettings = new AppSettings("./Data/patient.json", "./Data/doctor.json", "./Data/secretary.json",
-				"./Data/admin.json" );
+                "./Data/admin.json");
             _institution = Institution.Instance(appSettings);
             _institution.loadAll();
-            
+
             _navigation = NavigationStore.Instance();
 
 
-            Patient p1 = new Patient();
-            p1.Email = "nestolol@gmail.com";
-            p1.Password = "pitajKonobara";
-            Institution.Instance().PatientRepository.GetPatients().Add(p1);
+            //Patient p1 = new Patient();
+            //p1.Email = "nestolol@gmail.com";
+            //p1.Password = "pitajKonobara";
+            //Institution.Instance().PatientRepository.GetPatients().Add(p1);
 
-            Doctor p2 = new Doctor();
-            p2.Email = "nestolol2@gmail.com";
-            p2.Password = "pitajKonobara2";
-            Institution.Instance().DoctorRepository.GetDoctors().Add(p2);
+            //Doctor p2 = new Doctor();
+            //p2.Email = "nestolol2@gmail.com";
+            //p2.Password = "pitajKonobara2";
+            //Institution.Instance().DoctorRepository.GetDoctors().Add(p2);
 
-            Patient p3 = new Patient();
-            p3.Email = "nestolol3@gmail.com";
-            p3.Password = "pitajKonobara3";
-            Institution.Instance().PatientRepository.GetPatients().Add(p3);
+            //Patient p3 = new Patient();
+            //p3.Email = "nestolol3@gmail.com";
+            //p3.Password = "pitajKonobara3";
+            //Institution.Instance().PatientRepository.GetPatients().Add(p3);
 
-            Patient p4 = new Patient();
-            p4.Email = "a";
-            p4.Password = "a";
-            Institution.Instance().PatientRepository.GetPatients().Add(p4);
-
+            //Patient p4 = new Patient();
+            //p4.Email = "a";
+            //p4.Password = "a";
+            //Institution.Instance().PatientRepository.GetPatients().Add(p4);
 
 
         }
