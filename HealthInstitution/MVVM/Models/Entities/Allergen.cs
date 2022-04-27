7c821cc1d9cd645edfc95dfb37751c40cd91fbd9
Hristina Adamovic <hristinacina@gmail.com>
2022-04-27 23:17:27 +0200

    Added constructors

--- HealthInstitution/MVVM/Models/Entities/Allergen.cs
+++ HealthInstitution/MVVM/Models/Entities/Allergen.cs
@@ -2,5 +2,22 @@
 {
     public class Allergen
     {
+        private int _id;
+        private string _name;
+
+        public int Id { get => this._id; set { this._id = value; } }
+        public string Name { get => this._name; set { this._name = value; } }
+
+        public Allergen()
+        {
+            
+        }
+
+        public Allergen(int id, string name)
+        {
+            this._id = id;
+            this._name = name;
+        }
+
     }
 }
--- HealthInstitution/MVVM/Models/Entities/MedicalRecord.cs
+++ HealthInstitution/MVVM/Models/Entities/MedicalRecord.cs
@@ -9,14 +9,53 @@ namespace HealthInstitution.MVVM.Models.Entities
 {
     public class MedicalRecord
     {
-        public double Height;
-        public double Weight;
+        private double _height;
+        private double _weight;
+        private List<Allergen> _allergens;
+        private List<Appointment> _appointments;
+        // TODO: add refferals
+
+        public double Height { get => this._height; set { this._height = value; } }
+        public double Weight { get => this._weight; set { this._weight = value; } }
         // history of sickness !?
         [JsonIgnore]
-        public List<Allergen> Allergens;
+        public List<Allergen> Allergens{
+            get
+            {
+                if (this._allergens == null) this._allergens = new List<Allergen>();
+                return this._allergens;
+            }
+            set
+            {
+                this._allergens = value;
+            }
+        }
         [JsonIgnore]
-        public List<Appointment> Appointments;
+        public List<Appointment> Appointments{
+            get
+            {
+                if (this._appointments == null) this._appointments = new List<Appointment>();
+                return this._appointments;
+            }
+            set
+            {
+                this._appointments = value;
+            }
+        }
 
+        public MedicalRecord()
+        {
+        }
 
+        public MedicalRecord(double height, double weight)
+        {
+            this._height = height;
+            this._weight = weight;
+        }
+
+        public MedicalRecord(double height, double weight, List<Allergen> allergens) : this(height, weight)
+        {
+            this._allergens = allergens;
+        }
     }
 }
