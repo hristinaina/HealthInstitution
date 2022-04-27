ed879a78dc830ca5da00a13c42e468012db8b2fb
Nemanja Dutina <nemanjadutina@gmail.com>
2022-04-27 17:58:11 +0200

    fix: Created private attributes with properties

--- HealthInstitution/MVVM/Models/Entities/EquipmentArragment.cs
+++ HealthInstitution/MVVM/Models/Entities/EquipmentArragment.cs
@@ -9,11 +9,16 @@ namespace HealthInstitution.MVVM.Models.Entities
 {
     public class EquipmentArragment
     {
-        public int RoomId { get; set; }
-        public int EquipmentId { get; set; }
-        public int Quantity { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        private int _roomId;
+        private int _equipmentId;
+        private int _quantity;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        public int RoomId { get => this._roomId; set { this._roomId = value; } }
+        public int EquipmentId { get => this._equipmentId; set { this._equipmentId = value; } }
+        public int Quantity { get => this._quantity; set { this._quantity = value; } }
+        public DateTime StartDate { get => this._startDate; set { this._startDate = value; } }
+        public DateTime EndDate { get => this._endDate; set { this._endDate = value; } }
         public EquipmentArragment()
         {
 
