f761d78d94a5800859ccb32208233f3e6fac4515
Nemanja Dutina <nemanjadutina@gmail.com>
2022-04-27 20:19:04 +0200

    Created RoomRepository

--- HealthInstitution/MVVM/Models/Entities/Room/Room.cs
+++ HealthInstitution/MVVM/Models/Entities/Room/Room.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HealthInstitution.MVVM.Models.Enumerations;
+using HealthInstitution.MVVM.Models.Repositories;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,121 @@ namespace HealthInstitution.MVVM.Models.Entities
 {
     public class Room
     {
+        private int _id;
+        private string _name;
+        private int _number;
+        private RoomType _type;
+        private Dictionary<Equipment, int> _equipment;
+
+        public int ID { get => this._id; set { this._id = value; } }
+        public string Name { get => this._name; set { this._name = value; } }
+        public int Number { get => this._number; set { this._number = value; } }
+        public RoomType Type { get => this._type; set { this._type = value; } }
+
+        [JsonIgnore]
+        public Dictionary<Equipment, int> Equipment { get => this._equipment; set { this._equipment = value; } }
+
+        public Room()
+        {
+            this.Equipment = new Dictionary<Equipment, int>();
+        }
+
+        private Room(int id, int number, string name, RoomType type) : this()
+        {
+            this._id = id;
+            this._number = number;
+            this._name = name;
+            this._type = type;
+        }
+
+        public static Room Create(int id, int number, string name, RoomType type)
+        {
+            return new Room(id, number, name, type);
+        }
+
+        private static bool CheckID(List<Room> rooms, int id)
+        {
+            foreach (Room r in rooms)
+            {
+                if (r.ID == id) return false;
+            }
+            return true;
+        }
+
+        public static int GetID(List<Room> rooms)
+        {
+            int i = 1;
+            while (true)
+            {
+                if (CheckID(rooms, i)) return i;
+                i++;
+            }
+        }
+
+
+        public void ChangeNumber(RoomRepository repository, int newNumber)
+        {
+            if (repository.CheckNumber(newNumber)) this.Number = newNumber;
+            //Add RoomNumberAlreadyInUseException
+            else throw new Exception();
+        }
+
+        //private bool CheckType<T>(List<T> schedule)
+        //{
+        //    foreach (T t in schedule)
+        //    {
+        //        if (t.ID == this.ID) return false;
+        //    }
+        //    return true;
+        //}
+
+        public void ChangeType(OperationRepository operations, ExaminationRepository examinations, RoomType newType)
+        {
+            if (this.Type == RoomType.EXAM_ROOM)
+            {
+                //Check exams
+            }
+            else if (this.Type == RoomType.OPERATING_ROOM)
+            {
+                //Check operations
+            }
+            this.Type = newType;
+        }
+
+        public bool DeletionCheck(OperationRepository operations, ExaminationRepository examinations)
+        {
+            if (this.Type == RoomType.OPERATING_ROOM)
+            {
+                //Check exams
+            }
+            else if (this.Type == RoomType.EXAM_ROOM)
+            {
+                //Check operations
+            }
+            return true;
+        }
+
+        public void AddEquipment(Equipment e, int quantity)
+        {
+            if (!this.Equipment.ContainsKey(e))
+            {
+                this.Equipment[e] = 0;
+            }
+            this.Equipment[e] += quantity;
+        }
+
+        public void RemoveEquipment(Equipment e, int quantity)
+        {
+            if (!this.Equipment.ContainsKey(e))
+            {
+                //add NotInRoomException
+            }
+            this.Equipment[e] -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + this.ID + "; Number: " + this.Number + "; Name: " + this.Name + "; Tip: " + this.Type;
+        }
     }
 }
--- HealthInstitution/MVVM/Models/Repositories/Room/RoomRepository.cs
+++ HealthInstitution/MVVM/Models/Repositories/Room/RoomRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HealthInstitution.MVVM.Models.Entities;
+using HealthInstitution.MVVM.Models.Enumerations;
+using HealthInstitution.MVVM.Models.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,53 @@ namespace HealthInstitution.MVVM.Models.Repositories
 {
     public class RoomRepository
     {
+        private string _fileName;
+        private List<Room> _rooms;
+
+        public List<Room> Rooms {get => this._rooms;}
+
+        public RoomRepository(string roomsFileName)
+        {
+            this._fileName = roomsFileName;
+            this._rooms = new List<Room>();
+        }
+
+        public void LoadFromFile()
+        {
+            this._rooms = FileService.Deserialize<Room>(this._fileName);
+        }
+
+        public void SaveToFile()
+        {
+            FileService.Serialize<Room>(this._fileName, this._rooms);
+        }
+
+        public Room GetById(int id)
+        {
+            foreach (Room r in this._rooms)
+            {
+                if (r.ID == id) return r;
+            }
+            return null;
+        }
+
+        public bool CheckNumber(int number)
+        {
+            foreach (Room r in this._rooms)
+            {
+                if (r.Number == number) return false;
+            }
+            return true;
+        }
+
+        public List<Room> FilterByRoomType(RoomType type)
+        {
+            List<Room> filteredRooms = new List<Room>();
+            foreach (Room r in this._rooms)
+            {
+                if (r.Type == type) filteredRooms.Add(r);
+            }
+            return filteredRooms;
+        }
     }
 }
