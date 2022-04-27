b2eec66921d184cc41b39b9828295ed7547fa05c
Nemanja Dutina <nemanjadutina@gmail.com>
2022-04-27 22:36:31 +0200

    Created MedicineRepository

--- HealthInstitution/MVVM/Models/Repositories/Medicine/MedicineRepository.cs
+++ HealthInstitution/MVVM/Models/Repositories/Medicine/MedicineRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HealthInstitution.MVVM.Models.Entities;
+using HealthInstitution.MVVM.Models.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,32 @@ namespace HealthInstitution.MVVM.Models.Repositories
 {
     public class MedicineRepository
     {
+        private string _fileName;
+        private List<Medicine> _medicine;
+
+        public List<Medicine> Medicine { get => this._medicine; }
+        public MedicineRepository(string fileName)
+        {
+            this._fileName = fileName;
+            this._medicine = new List<Medicine>();
+        }
+
+        public void LoadFromFile()
+        {
+            _medicine = FileService.Deserialize<Medicine>(this._fileName);
+        }
+
+        public void SaveToFile()
+        {
+            FileService.Serialize<Medicine>(this._fileName, this._medicine);
+        }
+        public Medicine FindByID(int id)
+        {
+            foreach (Medicine medicine in _medicine)
+            {
+                if (medicine.ID == id) return medicine;
+            }
+            return null;
+        }
     }
 }
