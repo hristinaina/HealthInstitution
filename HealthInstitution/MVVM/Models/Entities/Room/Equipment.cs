0e4aada97e72cd408615b0e2358358be1384b4ea
Nemanja Dutina <nemanjadutina@gmail.com>
2022-04-27 20:32:43 +0200

    Created EquipmentRepository

--- HealthInstitution/MVVM/Models/Entities/Room/Equipment.cs
+++ HealthInstitution/MVVM/Models/Entities/Room/Equipment.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HealthInstitution.MVVM.Models.Enumerations;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,72 @@ namespace HealthInstitution.MVVM.Models.Entities
 {
     public class Equipment
     {
+        private int _id;
+        private string _name;
+        private int _quantity;
+        private EquipmentType _type;
+        private Dictionary<Room, int> _arrangmentByRooms;
+
+        public int ID { get => _id; set { this._id = value; } }
+        public string Name { get => this._name; set { this._name = value; } }
+        public int Quantity { get => this._quantity; set { this._quantity = value; } }
+        public EquipmentType Type { get => this._type; set { this._type = value; } }
+        [JsonIgnore]
+        public Dictionary<Room, int> ArrangmentByRooms
+        {
+            get
+            {
+                if (this._arrangmentByRooms == null) this._arrangmentByRooms = new Dictionary<Room, int>();
+                return this._arrangmentByRooms;
+            }
+            set
+            {
+                this._arrangmentByRooms = value;
+            }
+        }
+
+        public Equipment()
+        {
+
+        }
+
+        public Equipment(int id, string name, int quantity, EquipmentType type)
+        {
+            this._id = id;
+            this._name = name;
+            this._quantity = quantity;
+            this._type = type;
+        }
+
+        public Equipment(int id, string name, int quantity, EquipmentType type, Dictionary<Room, int> arragment) : this(id, name, quantity, type)
+        {
+            this._arrangmentByRooms = arragment;
+        }
+
+        public void ArrangeInRoom(Room r, int quantity)
+        {
+            if (!this._arrangmentByRooms.ContainsKey(r))
+            {
+                this._arrangmentByRooms[r] = 0;
+            }
+            this._arrangmentByRooms[r] += quantity;
+        }
+
+        public static List<Equipment> Search(List<Equipment> equipment, string phrase)
+        {
+            List<Equipment> matchingEquipment = new List<Equipment>();
+            foreach (Equipment e in equipment)
+            {
+                if (e.Name.Contains(phrase)) matchingEquipment.Add(e);
+            }
+            return matchingEquipment;
+        }
+
+
+        //public static List<Equipment> FilterByRoomType(List<Equipment> allEquipment, RoomType type)
+        //{
+        //    return new List<Equipment>();
+        //}
+
     }
 }
--- HealthInstitution/MVVM/Models/Repositories/Room/EquipmentRepository.cs
+++ HealthInstitution/MVVM/Models/Repositories/Room/EquipmentRepository.cs
@@ -1,6 +1,61 @@
-﻿namespace HealthInstitution.MVVM.Models
+﻿using HealthInstitution.MVVM.Models.Entities;
+using HealthInstitution.MVVM.Models.Enumerations;
+using HealthInstitution.MVVM.Models.Services;
+using System.Collections.Generic;
+
+namespace HealthInstitution.MVVM.Models
 {
     public class EquipmentRepository
     {
+        private string _fileName;
+        private List<Equipment> _equipment;
+
+        public List<Equipment> Equipment { get => this._equipment; }
+
+        public EquipmentRepository(string roomsFileName)
+        {
+            this._fileName = roomsFileName;
+            this._equipment = new List<Equipment>();
+        }
+
+        public void LoadFromFile()
+        {
+            this._equipment = FileService.Deserialize<Equipment>(this._fileName);
+        }
+
+        public void SaveToFile()
+        {
+            FileService.Serialize<Equipment>(this._fileName, this._equipment);
+        }
+
+        public Equipment GetById(int id)
+        {
+            foreach (Equipment e in this._equipment)
+            {
+                if (e.ID == id) return e;
+            }
+            return null;
+        }
+
+        public List<Equipment> FilterByEquipmentType(EquipmentType type)
+        {
+            List<Equipment> filteredEquipment = new List<Equipment>();
+
+            foreach (Equipment e in this._equipment)
+            {
+                if (e.Type == type) filteredEquipment.Add(e);
+            }
+            return filteredEquipment;
+        }
+
+        public static Dictionary<Room, Equipment> FilterByQuantity(Room r, List<Equipment> allEquipment, int minQuantity, int maxQuantity)
+        {
+            Dictionary<Room, Equipment> filteredEquipment = new Dictionary<Room, Equipment>();
+            foreach (Equipment e in allEquipment)
+            {
+                if (e.ArrangmentByRooms[r] >= minQuantity && e.ArrangmentByRooms[r] <= maxQuantity) filteredEquipment[r] = e;
+            }
+            return filteredEquipment;
+        }
     }
 }
