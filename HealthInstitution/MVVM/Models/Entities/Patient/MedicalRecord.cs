c2e4c915bb1e89926bfc1811c6f5ea07cc3f8a97
Hristina Adamovic <hristinacina@gmail.com>
2022-04-28 02:59:12 +0200

    Changed some properties and constructors

--- HealthInstitution/MVVM/Models/Entities/Patient/MedicalRecord.cs
+++ HealthInstitution/MVVM/Models/Entities/Patient/MedicalRecord.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using HealthInstitution.MVVM.Models.Entities.References;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,14 +10,65 @@ namespace HealthInstitution.MVVM.Models.Entities
 {
     public class MedicalRecord
     {
-        public double Height;
-        public double Weight;
+        private double _height;
+        private double _weight;
+        private List<Allergen> _allergens;
+        private List<Appointment> _appointments;
+        private List<Refferal> _refferals;
+        // TODO: add refferals
+
+        public double Height { get => _height; set { _height = value; } }
+        public double Weight { get => _weight; set { _weight = value; } }
         // history of sickness !?
         [JsonIgnore]
-        public List<Allergen> Allergens;
+        public List<Allergen> Allergens
+        {
+            get
+            {
+                if (_allergens == null) _allergens = new List<Allergen>();
+                return _allergens;
+            }
+            set
+            {
+                _allergens = value;
+            }
+        }
         [JsonIgnore]
-        public List<Appointment> Appointments;
+        public List<Appointment> Appointments
+        {
+            get
+            {
+                if (_appointments == null) _appointments = new List<Appointment>();
+                return _appointments;
+            }
+            set
+            {
+                _appointments = value;
+            }
+        }
+        [JsonIgnore]
+        public List<Refferal> Refferals
+        {
+            get
+            {
+                if (_refferals == null) _refferals = new List<Refferal>();
+                return _refferals;
+            }
+            set
+            {
+                _refferals = value;
+            }
+        }
 
+        public MedicalRecord()
+        {
+        }
 
+        public MedicalRecord(double height, double weight, List<Allergen> allergens = null)
+        {
+            _height = height;
+            _weight = weight;
+            _allergens = allergens;
+        }
     }
 }
