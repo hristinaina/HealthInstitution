39b74bd860209a629a9c000352d956d5a1dcf9d9
Hristina Adamovic <hristinacina@gmail.com>
2022-04-28 15:17:36 +0200

    Added file paths

--- HealthInstitution/AppSettings.cs
+++ HealthInstitution/AppSettings.cs
@@ -10,29 +10,51 @@ namespace HealthInstitution
 	// implemented using Singleton pattern
 	public sealed class AppSettings
     {
-        private string _patientFileName;
-        private string _doctorFileName;
-        private string _secretaryFileName;
-        private string _adminFileName;
-        private string _examinationFileName;
-        private string _perscriptionFileName;
-        private string _operationFileName;
-        private string _examinationReferenceFileName;
-        private string _operationReferenceFileName;
+
+        private string _patientsFileName;
+        private string _doctorsFileName;
+        private string _secretariesFileName;
+        private string _adminsFileName;
+        private string _examinationsFileName;
+        private string _perscriptionsFileName;
+        private string _operationsFileName;
+        private string _examinationsReferencesFileName;
+        private string _operationsReferencesFileName;
+
+        private string _allergensFileName;
+        private string _medicineAllergensFileName;
+        private string _patientAllergensFileName;
 
         private string _equipmentFileName;
-        private string _roomFileName;
-        private string _equipmentArragmentFileName;
-        private string _medicineFileName;
-        private string _dayOffFileName;
+        private string _roomsFileName;
+        private string _equipmentArrangementFileName;
+        private string _medicinesFileName;
+        private string _daysOffFileName;
         private string _refferalFileName;
-        // TODO add the rest of the class attributes that will store the name of the corresponding files
+        
+        public string PatientsFileName { get => _patientsFileName; set => _patientsFileName = value; }
+        public string DoctorsFileName { get => _doctorsFileName; set => _doctorsFileName = value; }
+        public string DaysOffFileName { get => _daysOffFileName; set => _daysOffFileName = value; }
+        public string SecretariesFileName { get => _secretariesFileName; set => _secretariesFileName = value; }
+        public string AdminsFileName { get => _adminsFileName; set => _adminsFileName = value; }
+        public string ExaminationsFileName { get => _examinationsFileName; set => _examinationsFileName = value; }
+        public string OperationsFileName { get => _operationsFileName; set => _operationsFileName = value; }
+        public string ExaminationReferencesFileName { get => _examinationsReferencesFileName; set => _examinationsReferencesFileName = value; }
+        public string OperationsReferencesFileName { get => _operationsReferencesFileName; set => _operationsReferencesFileName = value; }
+        public string PerscriptionsFileName { get => _perscriptionsFileName; set => _perscriptionsFileName = value; }
+        public string RefferalsFileName { get => _refferalFileName; set => _refferalFileName = value; }
+        public string EquipmentFileName { get => _equipmentFileName; set => _equipmentFileName = value; }
+        public string EquipmentArrangementFileName { get => _equipmentArrangementFileName; set => _equipmentFileName = value; }
+        public string RoomsFileName { get => _roomsFileName; set => _roomsFileName = value; }
+        public string MedicinesFileName { get => _medicinesFileName; set => _medicinesFileName = value; }
+        public string AllergensFileName { get => _allergensFileName; set => _allergensFileName = value; }
+        public string PatientAllergensFileName { get => _patientAllergensFileName; set => _patientAllergensFileName = value; }
+        public string MedicineAllergensFileName { get => _medicineAllergensFileName; set => _medicineAllergensFileName = value; }
+
 
         private AppSettings() { }
 
         private static AppSettings s_instance = null;
-
-
 
         public static AppSettings Instance()
         {
@@ -43,105 +65,46 @@ namespace HealthInstitution
             return s_instance;
         }
 
+        public static void SetInstance(AppSettings instance)
+        {
+            if (s_instance is null)
+            {
+                s_instance = instance;
+            }
+        }
+
         public void AddFilePaths(string patientFileName, string doctorFileName, string secretaryFileName, string adminFileName,
                                  string examinationFileName, string operationFileName, string examinationReferenceFileName, string operationReferenceFileName, string equipmentFileName,
-                                 string roomFileName, string equipmentArragmentFileName, string medicineFileName, string dayOffFileName, string perscriptionFileName, string refferalFileName)
+                                 string roomFileName, string equipmentArragmentFileName, string medicineFileName, string dayOffFileName, string perscriptionFileName, string refferalFileName,
+                                 string allergenFileName, string medicineAllergenFileName, string patientAllergenFileName)
         {
-            _patientFileName = patientFileName;
-            _doctorFileName = doctorFileName;
-            _secretaryFileName = secretaryFileName;
-            _adminFileName = adminFileName;
+            _patientsFileName = patientFileName;
+            _doctorsFileName = doctorFileName;
+            _secretariesFileName = secretaryFileName;
+            _adminsFileName = adminFileName;
 
-            _examinationFileName = examinationFileName;
-            _perscriptionFileName = perscriptionFileName;
-            _operationFileName = operationFileName;
-            _examinationReferenceFileName = examinationReferenceFileName;
-            _operationReferenceFileName = examinationReferenceFileName;
+            _examinationsFileName = examinationFileName;
+            _perscriptionsFileName = perscriptionFileName;
+            _operationsFileName = operationFileName;
+            _examinationsReferencesFileName = examinationReferenceFileName;
+            _operationsReferencesFileName = examinationReferenceFileName;
 
-            _roomFileName = roomFileName;
+            _allergensFileName = allergenFileName;
+            _medicineAllergensFileName = medicineAllergenFileName;
+            _patientAllergensFileName = patientAllergenFileName;
+
+            _roomsFileName = roomFileName;
             _equipmentFileName = equipmentFileName;
-            _equipmentArragmentFileName = equipmentArragmentFileName;
+            _equipmentArrangementFileName = equipmentArragmentFileName;
 
-            _medicineFileName = medicineFileName;
+            _medicinesFileName = medicineFileName;
 
-            _dayOffFileName = dayOffFileName;
+            _daysOffFileName = dayOffFileName;
 
             _refferalFileName = refferalFileName;
 
             // TODO add the rest of the class attributes
         }
 
-        public string GetAdminFileName()
-        {
-            return _adminFileName;
-        }
-
-        public string GetPatientFileName()
-        {
-            return _patientFileName;
-        }
-
-        public string GetDoctorFileName()
-        {
-            return _doctorFileName;
-        }
-
-        public string GetSecretaryFileName()
-        {
-            return _secretaryFileName;
-        }
-
-        internal string GetEquipmentArragmentFileName()
-        {
-            return this._equipmentArragmentFileName;
-        }
-
-        public string GetAppointmentFileName()
-        {
-            return _examinationFileName;
-        }
-
-        public string GetExationFileName() {
-            return _examinationFileName;
-        }
-
-        public string GetPerscriptionFileName()
-        {
-            return _perscriptionFileName;
-        }
-        public string GetOperationFileName()
-        {
-            return _operationFileName;
-        }
-
-        public string GetExaminationReferenceFileName()
-        {
-            return _examinationReferenceFileName;
-        }
-        public string GetOperationReferenceFileName()
-        {
-            return _operationReferenceFileName;
-        }
-        public string GetEquipmentFileName()
-        {
-            return _equipmentFileName;
-        }
-        public string GetRoomFileName()
-        {
-            return _roomFileName;
-        }
-        public string GetMedicineFileName()
-        {
-            return _medicineFileName;
-        }
-        public string GetDayOffFileName()
-        {
-            return _dayOffFileName;
-        }
-
-        public string GetRefferalFileName()
-        {
-            return _refferalFileName;
-        }
     }
 }
