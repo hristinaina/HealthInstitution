a86afc5d25a00d2ada48eb922bcb566ae0a61f0a
Hristina Adamovic <hristinacina@gmail.com>
2022-04-28 12:57:08 +0200

    Added changes from develop branch

--- HealthInstitution/MVVM/Models/Entities/Medicine/Allergen.cs
+++ HealthInstitution/MVVM/Models/Entities/Medicine/Allergen.cs
@@ -1,6 +1,29 @@
-﻿namespace HealthInstitution.MVVM.Models.Entities
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.MVVM.Models.Entities
 {
     public class Allergen
     {
+        private int _id;
+        private string _name;
+
+        public int Id { get => _id; set { _id = value; } }
+        public string Name { get => _name; set { _name = value; } }
+
+        public Allergen()
+        {
+
+        }
+
+        public Allergen(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
     }
 }
