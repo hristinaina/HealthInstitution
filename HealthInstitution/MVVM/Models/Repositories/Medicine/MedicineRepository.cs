95e43a68af6ae8745404517efb40a9f561e23805
Hristina Adamovic <hristinacina@gmail.com>
2022-04-28 12:54:13 +0200

    Bug fix

--- HealthInstitution/MVVM/Models/Repositories/Medicine/MedicineRepository.cs
+++ HealthInstitution/MVVM/Models/Repositories/Medicine/MedicineRepository.cs
@@ -1,10 +1,6 @@
-﻿using HealthInstitution.MVVM.Models.Entities;
+﻿using System.Collections.Generic;
+using HealthInstitution.MVVM.Models.Entities;
 using HealthInstitution.MVVM.Models.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HealthInstitution.MVVM.Models.Repositories
 {
