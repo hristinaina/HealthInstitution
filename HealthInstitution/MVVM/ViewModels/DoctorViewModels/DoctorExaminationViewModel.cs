c0a656301ce1c712489af2f2e46829ca207cc9f0
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-04-29 21:16:39 +0200

    Added view models for examinations
    Added DoctorExaminationViewModel and ExaminationViewModel.
    Still missing commands for buttons.

---
+++ HealthInstitution/MVVM/ViewModels/DoctorViewModels/DoctorExaminationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+
+namespace HealthInstitution.MVVM.ViewModels.DoctorViewModels
+{
+    class DoctorExaminationViewModel : BaseViewModel
+    {
+        private readonly ObservableCollection<ExaminationViewModel> _examinations;
+
+        public IEnumerable<ExaminationViewModel> Examinations => _examinations;
+        public ICommand ScheduleExaminationCommand { get; }
+
+        public DoctorExaminationViewModel()
+        {
+            _examinations = new ObservableCollection<ExaminationViewModel>();
+        }
+    }
+}
