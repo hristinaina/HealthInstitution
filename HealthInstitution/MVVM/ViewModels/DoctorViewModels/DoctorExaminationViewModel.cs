5287b334732e37cc742e619f34e671cd11ed248f
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-04-29 22:21:43 +0200

    Added view model for medical record

--- HealthInstitution/MVVM/ViewModels/DoctorViewModels/DoctorExaminationViewModel.cs
+++ HealthInstitution/MVVM/ViewModels/DoctorViewModels/DoctorExaminationViewModel.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using System.Collections.ObjectModel;
+using HealthInstitution.MVVM.Models;
 
 namespace HealthInstitution.MVVM.ViewModels.DoctorViewModels
 {
@@ -13,11 +14,22 @@ namespace HealthInstitution.MVVM.ViewModels.DoctorViewModels
         private readonly ObservableCollection<ExaminationViewModel> _examinations;
 
         public IEnumerable<ExaminationViewModel> Examinations => _examinations;
-        public ICommand ScheduleExaminationCommand { get; }
 
-        public DoctorExaminationViewModel()
+        public ICommand ScheduleExaminationCommand { get; }
+        public ICommand StartExamination { get; }
+        public ICommand MedicalRecord { get; }
+        public ICommand UpdateExamination { get; }
+        public ICommand DeleteExamination { get; }
+        public ICommand CreateExamination { get; }
+        public ICommand CancelExamination { get; }
+
+        public DoctorExaminationViewModel(ExaminationRepository examinationRepository)
         {
             _examinations = new ObservableCollection<ExaminationViewModel>();
+
+            // add here test examples
+
+             
         }
     }
 }
