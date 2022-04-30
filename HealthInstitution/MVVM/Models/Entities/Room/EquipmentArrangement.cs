e8d195f23d1f63c81fbb15257a40be764e1e23a1
Nemanja Dutina <nemanjadutina@gmail.com>
2022-04-30 20:59:46 +0200

    Code clean up: Removing this.
    Removing this. from classes

--- HealthInstitution/MVVM/Models/Entities/Room/EquipmentArrangement.cs
+++ HealthInstitution/MVVM/Models/Entities/Room/EquipmentArrangement.cs
@@ -15,11 +15,11 @@ namespace HealthInstitution.MVVM.Models.Entities
         private DateTime _startDate;
         private DateTime _endDate;
 
-        public int RoomId { get => this._roomId; set { this._roomId = value; } }
-        public int EquipmentId { get => this._equipmentId; set { this._equipmentId = value; } }
-        public int Quantity { get => this._quantity; set { this._quantity = value; } }
-        public DateTime StartDate { get => this._startDate; set { this._startDate = value; } }
-        public DateTime EndDate { get => this._endDate; set { this._endDate = value; } }
+        public int RoomId { get => _roomId; set => _roomId = value; }
+        public int EquipmentId { get => _equipmentId; set => _equipmentId = value; }
+        public int Quantity { get => this._quantity; set => _quantity = value; }
+        public DateTime StartDate { get => this._startDate; set => _startDate = value; }
+        public DateTime EndDate { get => this._endDate; set => _endDate = value; }
         public EquipmentArrangement()
         {
 
