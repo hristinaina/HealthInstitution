e648a204e28085a1367ac83a4be66e8a56b63400
Hristina Adamovic <hristinacina@gmail.com>
2022-04-30 15:32:48 +0200

    Created commands

---
+++ HealthInstitution/MVVM/ViewModels/Commands/SecretaryCommands/BlockCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using HealthInstitution.Commands;
+using HealthInstitution.MVVM.Models;
+using HealthInstitution.MVVM.Models.Entities;
+using HealthInstitution.MVVM.ViewModels.SecretaryViewModels;
+
+namespace HealthInstitution.MVVM.ViewModels.Commands.SecretaryCommands
+{
+    public class BlockCommand : BaseCommand
+    {
+        private readonly Institution _institution;
+        private PatientListViewModel _viewModel;
+
+        public BlockCommand(PatientListViewModel viewModel)
+        {
+            _institution = Institution.Instance();
+            _viewModel = viewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            Patient patient = Institution.Instance().PatientRepository.FindByID(_viewModel.SelectedPatientId);
+            patient.BlockPatient();
+            string message = "The patient has ben successfully blocked.";
+            MessageBox.Show(message);
+            _viewModel.FillPatientList();
+        }
+    }
+}
