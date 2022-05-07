0d05c34870fafa35c0eefb44e7cce7674b4ff6ce
Nemanja Dutina <nemanjadutina@gmail.com>
2022-05-08 00:40:49 +0200

    Created RoomsRenovation entity
    RoomRenovation is used to connect room ids and renovation ids

---
+++ HealthInstitution/MVVM/Models/Entities/Room/RoomsRenovation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthInstitution.MVVM.Models.Entities.Room
+{
+    public class RoomsRenovation
+    {
+        private int _renovationId;
+        private int _roomId;
+        private bool _result;
+
+        public int RenovationId { get => _renovationId; set => _renovationId = value; }
+        public int RoomId { get => _roomId; set => _roomId = value; }
+        public bool Result { get => _result; set => _result = value; }
+
+        public RoomsRenovation()
+        {
+
+        }
+
+        public RoomsRenovation(int renovationId, int roomId, bool result)
+        {
+            _renovationId = renovationId;
+            _roomId = roomId;
+            _result = result;
+        }
+    }
+}
