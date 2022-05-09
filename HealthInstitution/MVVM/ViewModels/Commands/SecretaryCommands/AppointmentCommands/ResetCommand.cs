00ee4f852013aa1a2ce50f5826e70a6d4ef691ea
Hristina Adamovic <hristinacina@gmail.com>
2022-05-10 00:21:27 +0200

    Created command

---
+++ HealthInstitution/MVVM/ViewModels/Commands/SecretaryCommands/AppointmentCommands/ResetCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthInstitution.Commands;
+using HealthInstitution.MVVM.ViewModels.SecretaryViewModels;
+
+namespace HealthInstitution.MVVM.ViewModels.Commands.SecretaryCommands.AppointmentCommands
+{
+    public class ResetCommand : BaseCommand
+    {
+        private AppointmentsViewModel _viewModel;
+
+        public ResetCommand(AppointmentsViewModel model)
+        {
+            _viewModel = model;
+        }
+        public override void Execute(object parameter)
+        {
+            _viewModel.SearchPhrase = null;
+            _viewModel.FillReferralsList();
+        }
+    }
+}
