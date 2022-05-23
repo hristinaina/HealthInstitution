d623d56510aa16a5b1df3a334626a91884e2baab
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-05-23 17:45:25 +0200

    Created PendingMedicineCommand
    Added new tab item in navigation.

---
+++ HealthInstitution/MVVM/ViewModels/DoctorViewModels/Medicine/DoctorPendingMedicineViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using HealthInstitution.MVVM.Models.Entities;
+using HealthInstitution.MVVM.ViewModels.Commands.DoctorCommands;
+using HealthInstitution.MVVM.Models;
+
+namespace HealthInstitution.MVVM.ViewModels.DoctorViewModels
+{
+    class DoctorPendingMedicineViewModel : BaseViewModel
+    {
+    }
+}
