d89b5cf9ed2f06ca913e35a10378c0f79a8ddf82
Hristina Adamovic <hristinacina@gmail.com>
2022-05-23 19:31:02 +0200

    Implemented view and view model

--- HealthInstitution/MVVM/ViewModels/SecretaryViewModels/ArrangingEquipmentViewModel.cs
+++ HealthInstitution/MVVM/ViewModels/SecretaryViewModels/ArrangingEquipmentViewModel.cs
@@ -4,19 +4,30 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HealthInstitution.MVVM.Models;
+using HealthInstitution.MVVM.Models.Entities;
+using HealthInstitution.MVVM.Models.Enumerations;
+using HealthInstitution.MVVM.ViewModels.AdminViewModels;
 
 namespace HealthInstitution.MVVM.ViewModels.SecretaryViewModels
 {
     public class ArrangingEquipmentViewModel : BaseViewModel
     {
         public SecretaryNavigationViewModel Navigation { get; }
+        private readonly ObservableCollection<EquipmentListItemViewModel> _equipmentArrangement;
+        public IEnumerable<EquipmentListItemViewModel> EquipmentArrangement => _equipmentArrangement;
 
-       // private readonly ObservableCollection<BlockedPatientItemViewModel> _patients;
-       // public IEnumerable<BlockedPatientItemViewModel> Patients => _patients;
-       // private BlockedPatientItemViewModel _selectedPatient;
+        private EquipmentListItemViewModel _selectedEquipment;
+        public EquipmentListItemViewModel SelectedEquipment { get => _selectedEquipment; set => _selectedEquipment = value; }
+
+        public string SelectedRoom { get; set; }
+        public string SelectedName { get; set; }
+        public string SelectedQuantity { get; set; }
+
+        private List<Room> _rooms;
+        public List<Room> Rooms => _rooms;
 
         private bool _enableChanges;
-        private int _selection;
         public bool EnableChanges
         {
             get => _enableChanges;
@@ -38,22 +49,86 @@ namespace HealthInstitution.MVVM.ViewModels.SecretaryViewModels
             }
         }
 
+        private int _selection;
         public int Selection
         {
             get => _selection;
             set
             {
-                if (value < 0) { return; };
+                if (value < 0) { return; }
                 _selection = value;
-                EnableChanges = true;
                 OnPropertyChanged(nameof(Selection));
+                _selectedEquipment = _equipmentArrangement.ElementAt(_selection);
+                SelectedRoom = _selectedEquipment.RoomNumber;
+                OnPropertyChanged(nameof(SelectedRoom));
+                SelectedName = _selectedEquipment.Name;
+                OnPropertyChanged(nameof(SelectedName));
+                SelectedQuantity = _selectedEquipment.Quantity;
+                OnPropertyChanged(nameof(SelectedQuantity));
+                EnableChanges = true;
+            }
+        }
+
+        private Room _newArrangementTargetRoom;
+        public Room NewArrangementTargetRoom
+        {
+            get => _newArrangementTargetRoom;
+            set
+            {
+                _newArrangementTargetRoom = value;
+                OnPropertyChanged(nameof(NewArrangementTargetRoom));
+            }
+        }
+
+        private int _newArrangementQuantity;
+        public int NewArrangementQuantity
+
+        {
+            get => _newArrangementQuantity;
+            set
+            {
+                _newArrangementQuantity = value;
+                OnPropertyChanged(nameof(NewArrangementQuantity));
             }
         }
 
         public ArrangingEquipmentViewModel()
         {
             Navigation = new SecretaryNavigationViewModel();
+            _equipmentArrangement = new ObservableCollection<EquipmentListItemViewModel>();
+            _rooms = new List<Room>();
             EnableChanges = false;
+
+            FillEquipmentArrangementList();
+            FillRooms();
+        }
+
+        private void FillRooms()
+        {
+            foreach (Room r in Institution.Instance().RoomRepository.Rooms)
+            {
+                _rooms.Add(r);
+            }
+        }
+
+        public void FillEquipmentArrangementList()
+        {
+            _equipmentArrangement.Clear();
+
+            foreach (Equipment e in Institution.Instance().EquipmentRepository.Equipment)
+            {
+                if (e.Type == EquipmentType.HALLWAY_EQUIPMENT || e.Type == EquipmentType.ROOM_FURNITURE) continue;
+                foreach (Room r in e.ArrangmentByRooms.Keys)
+                {
+                    _equipmentArrangement.Add(new EquipmentListItemViewModel(e, r));
+                }
+            }
+
+            if (_equipmentArrangement.Count != 0)
+            {
+                Selection = 0;
+                OnPropertyChanged(nameof(Selection));
+            }
         }
     }
 }
