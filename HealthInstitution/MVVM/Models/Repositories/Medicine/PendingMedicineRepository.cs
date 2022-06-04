2cb81c7b06f441d24eff348c5e5b07249679836c
Nemanja Dutina <dutina.sv27.2020@uns.ac.rs>
2022-06-04 20:56:44 +0200

    Medicine creation and revision test

--- HealthInstitution/MVVM/Models/Repositories/Medicine/PendingMedicineRepository.cs
+++ HealthInstitution/MVVM/Models/Repositories/Medicine/PendingMedicineRepository.cs
@@ -87,6 +87,7 @@ namespace HealthInstitution.MVVM.Models.Repositories
         public PendingMedicine ChangeMedicine(PendingMedicine medicine, string newName, List<Allergen> newIngredients)
         {
             if (newName is null || newName.Equals("")) throw new NameNotAvailableException("Name cannot be empty");
+            else if (newIngredients.Count == 0) throw new ZeroIngredientsException("Ingredient list cannot be empty");
             else if (!IsNameAvailable(medicine, newName)) throw new NameNotAvailableException("Name already in use!");
 
             List<Allergen> alreadyIngredient = medicine.Ingredients.Except(newIngredients).ToList();
