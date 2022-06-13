2302f6cc344e29a051aace9ed587d94ef28a8fd7
Hristina Adamovic <hristinacina@gmail.com>
2022-06-13 04:10:51 +0200

    Changed file path

--- HealthInstitution/MVVM/ViewModels/Commands/SecretaryCommands/DayOffCommands/AcceptRequestCommand.cs
+++ HealthInstitution/MVVM/ViewModels/Commands/SecretaryCommands/DayOffCommands/AcceptRequestCommand.cs
@@ -6,27 +6,29 @@ using System.Threading.Tasks;
 using System.Windows;
 using HealthInstitution.Commands;
 using HealthInstitution.MVVM.Models;
+using HealthInstitution.MVVM.Models.Services.DoctorServices;
 using HealthInstitution.MVVM.ViewModels.SecretaryViewModels;
 
 namespace HealthInstitution.MVVM.ViewModels.Commands.SecretaryCommands
 {
-    public class RejectRequestCommand : BaseCommand
+    public class AcceptRequestCommand : BaseCommand
     {
         private readonly Institution _institution;
         private DoctorDaysOffViewModel _viewModel;
+        private DayOffService _service;
 
-        public RejectRequestCommand(DoctorDaysOffViewModel viewModel)
+        public AcceptRequestCommand(DoctorDaysOffViewModel viewModel)
         {
             _institution = Institution.Instance();
             _viewModel = viewModel;
+            _service = new DayOffService();
         }
 
         public override void Execute(object parameter)
         {
             _viewModel.DialogOpen = false;
-            MessageBox.Show("The request has been rejected! Notification sent to doctor.");
-            // izmjeni status request-a
-            // posalji notifikaciju doktoru
+            MessageBox.Show("The request has been accepted! Notification sent to doctor.");
+            _service.AcceptRequest(_viewModel.SelectedRequest.ID);
             _viewModel.FillRequestsList();
         }
     }
