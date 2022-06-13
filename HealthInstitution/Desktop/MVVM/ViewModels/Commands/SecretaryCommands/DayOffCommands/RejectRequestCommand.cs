95740498a6b829b2d00371570058fd6af8a1aaf1
Hristina Adamovic <hristinacina@gmail.com>
2022-06-13 12:46:51 +0200

    Changed file path

--- HealthInstitution/Desktop/MVVM/ViewModels/Commands/SecretaryCommands/DayOffCommands/RejectRequestCommand.cs
+++ HealthInstitution/Desktop/MVVM/ViewModels/Commands/SecretaryCommands/DayOffCommands/RejectRequestCommand.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using HealthInstitution.Commands;
+using HealthInstitution.Core;
 using HealthInstitution.MVVM.Models;
 using HealthInstitution.MVVM.Models.Services.DoctorServices;
 using HealthInstitution.MVVM.ViewModels.SecretaryViewModels;
