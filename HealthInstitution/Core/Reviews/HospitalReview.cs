7c882f7e65ca4b7170133a111e81a144d6be16cb
Nemanja Dutina <dutina.sv27.2020@uns.ac.rs>
2022-06-14 13:18:02 +0200

    Created review model
    Created models for hospital and doctor reviews

---
+++ HealthInstitution/Core/Reviews/HospitalReview.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace HealthInstitution.Core
+{
+    public class HospitalReview : Review
+    {
+        private int _hygene;
+        private int _satisfaction;
+     
+        [JsonProperty("Hygene")]
+        public int Hygene { get => _hygene; set { _hygene = value; } }
+        [JsonProperty("Satisfaction")]
+        public int Satisfacion { get => _satisfaction; set { _satisfaction = value; } }
+
+
+        public HospitalReview(int service, int hygene, int satisfaction, int suggestion, string comment) : base(service, suggestion, comment)
+        {
+            _hygene = hygene;
+            _satisfaction = satisfaction;
+        }
+
+        public HospitalReview() : base()
+        {
+            _hygene = 0;
+            _satisfaction = 0;
+        }
+    }
+}
---
+++ HealthInstitution/Core/Reviews/Review.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+namespace HealthInstitution.Core
+{
+    public class Review
+    {
+        private int _service;
+        private int _suggestion;
+        private string _comment;
+
+        [JsonProperty("Service")]
+        public int Service { get => _service; set { _service = value; } }
+        [JsonProperty("Suggestion")]
+        public int Suggestion { get => _suggestion; set { _suggestion = value; } }
+        [JsonProperty("Comment")]
+        public string Comment { get => _comment; set { _comment = value; } }
+
+        public Review(int service, int suggestion, string comment)
+        {
+            _service = service;
+            _suggestion = suggestion;
+            _comment = comment;
+        }
+
+        public Review()
+        {
+            _service = 0;
+            _suggestion = 0;
+            _comment = " ";
+        }
+    }
+}
