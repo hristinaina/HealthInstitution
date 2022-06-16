cdc5234ced0506da0c89386425d131a58ab8c34c
kaca01 <96604086+kaca01@users.noreply.github.com>
2022-06-16 18:09:16 +0200

    Bugfix

--- HealthInstitution/Desktop/MVVM/Models/Services/DoctorServices/DayOffService.cs
+++ HealthInstitution/Desktop/MVVM/Models/Services/DoctorServices/DayOffService.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HealthInstitution.Core;
 using HealthInstitution.Core.Repositories;
+using HealthInstitution.Core.Repository;
 
 namespace HealthInstitution.Core.Services
 {
