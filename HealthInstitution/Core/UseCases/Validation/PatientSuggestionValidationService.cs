e28b8830db56024ab6b7b59c217603c09ea1b294
Milica Sladaković <milicasladakovic7@gmail.com>
2022-06-16 23:36:47 +0200

    Validated Surveys
    Added validation for surveys when a survey is not finished but submmited.

--- HealthInstitution/Core/UseCases/Validation/PatientSuggestionValidationService.cs
+++ HealthInstitution/Core/UseCases/Validation/PatientSuggestionValidationService.cs
@@ -19,14 +19,6 @@ namespace HealthInstitution.Core.UseCases.Validation
                 {
                     throw new EmptyFieldException("Doctor not selected !");
                 }
-                if (DateTime.Compare(DateTime.Now, query.StartTime) > 0)
-                {
-                    throw new DateException("Start date must be in future !");
-                }
-                if (DateTime.Compare(DateTime.Now, query.EndTime) > 0)
-                {
-                    throw new DateException("End must be in future !");
-                }
                 if (DateTime.Compare(DateTime.Now, query.DeadlineDate) > 0)
                 {
                     throw new DateException("Deadline must be in future !");
