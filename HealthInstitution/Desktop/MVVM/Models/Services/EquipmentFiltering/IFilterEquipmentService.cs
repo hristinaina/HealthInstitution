f15c821fe1803578cb75b97f42f7bc9cdffd40c2
Nemanja Dutina <dutina.sv27.2020@uns.ac.rs>
2022-06-16 21:52:50 +0200

    Merge branch 'refactor-admin-services' into develop

--- HealthInstitution/Desktop/MVVM/Models/Services/EquipmentFiltering/IFilterEquipmentService.cs
+++ HealthInstitution/Desktop/MVVM/Models/Services/EquipmentFiltering/IFilterEquipmentService.cs
@@ -4,6 +4,7 @@ namespace HealthInstitution.Core.Services.Equipments
 {
     public interface IFilterEquipmentService
     {
-        public Dictionary<Equipment, List<Room>> Filter(RoomType roomType, int minQuantity, int maxQuantity, EquipmentType equipmentType)
+        public Dictionary<Equipment, List<Room>> Filter(RoomType roomType, int minQuantity, int maxQuantity,
+            EquipmentType equipmentType);
     }
 }
