05c0a20568bbfb0d87006d5e51ca50afb34af0fe
Hristina Adamovic <hristinacina@gmail.com>
2022-07-04 21:02:52 +0200

    Removed unused imports

--- HealthInstitution/DesktopHost/Controllers/SecretaryViewModels/AppointmentsViewModel.cs
+++ HealthInstitution/DesktopHost/Controllers/SecretaryViewModels/AppointmentsViewModel.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using HealthInstitution.Core;
 using HealthInstitution.Core;
 using HealthInstitution.Core.Repository;
 using HealthInstitution.Core.Services;
--- HealthInstitution/DesktopHost/Controllers/SecretaryViewModels/EmergencyAppointmentViewModel.cs
+++ HealthInstitution/DesktopHost/Controllers/SecretaryViewModels/EmergencyAppointmentViewModel.cs
@@ -2,15 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using HealthInstitution.Core;
 using HealthInstitution.Core;
 using HealthInstitution.Core.Services;
 using HealthInstitution.MVVM.ViewModels.Commands.SecretaryCommands.AppointmentCommands;
 using HealthInstitution.MVVM.ViewModels.PatientViewModels;
-using HealthInstitution.Core.Services;
 
 namespace HealthInstitution.MVVM.ViewModels.SecretaryViewModels
 {
